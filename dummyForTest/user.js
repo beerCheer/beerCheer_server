62809a42addf324d6e20867f3aa55cc05f8b96c0
sohhyeonkim <sohhyeonkim@gmail.com>
2022-04-23 16:18:05 +0900

    🧹 chore: 테스트용 dummy data 생성 및 적용

---
+++ dummyForTest/user.js
@@ -0,0 +1,32 @@
+const users = [
+  {
+    nickname: "test1",
+    email: "test1@test.com",
+    isPreferenceOrRateChecked: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    nickname: "test2",
+    email: "test2@test.com",
+    isPreferenceOrRateChecked: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    nickname: "test3",
+    email: "test3@test.com",
+    isPreferenceOrRateChecked: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    nickname: "test4",
+    email: "test4@test.com",
+    isPreferenceOrRateChecked: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+module.exports = users;

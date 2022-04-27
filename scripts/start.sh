c3967e78a8ac56d02c0c82251b15a1ed3aafcca7
sohhyeonkim <sohhyeonkim@gmail.com>
2022-04-27 17:01:33 +0900

    🧹 chore: start.sh 환경변수명 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/beerCheer_server/bin
 
-export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_USERNAME --query Parameters[0].Value | sed 's/"//g')
-export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_PASSWORD --query Parameters[0].Value | sed 's/"//g')
-export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_NAME --query Parameters[0].Value | sed 's/"//g')
-export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_HOST --query Parameters[0].Value | sed 's/"//g')
+export DEV_DB_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_USERNAME --query Parameters[0].Value | sed 's/"//g')
+export DEV_DB_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DEV_DB_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_NAME --query Parameters[0].Value | sed 's/"//g')
+export DEV_DB_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_HOST --query Parameters[0].Value | sed 's/"//g')
+export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value | sed 's/"//g')
 authbind --deep pm2 start server.js -f

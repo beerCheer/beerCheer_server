1bf00227581f4327b44c4e7cbe363697b38d448b
sohhyeonkim <sohhyeonkim@gmail.com>
2022-04-28 12:28:33 +0900

    ✨ feat: bEER-35] 네이버 로그인 (in progress)
    N

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,7 @@ export DEV_DB_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names 
 export DEV_DB_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_NAME --query Parameters[0].Value | sed 's/"//g')
 export DEV_DB_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DEV_DB_HOST --query Parameters[0].Value | sed 's/"//g')
 export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value | sed 's/"//g')
+export BEER_NAVER_CLIENTID=$(aws ssm get-parameters --region ap-northeast-2 --names BEER_NAVER_CLIENTID --query Parameters[0].Value | sed 's/"//g')
+export BEER_NAVER_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names BEER_NAVER_CLIENT_SECRET --query Parameters[0].Value | sed 's/"//g')
+
 authbind --deep pm2 start server.js -f
